5cc7ed05029d76897195b4fde60959d79bc8a68e
xukj <lovejoefirst@sina.cn>
2018-03-20 19:50:36 +0800

    邮件内容改为模板

--- src/main/java/com/bonc/dem/service/MailService.java
+++ src/main/java/com/bonc/dem/service/MailService.java
@@ -1,8 +1,5 @@
 package com.bonc.dem.service;
 
 public interface MailService {
-    void sendSimpleMail (String to, String subject, String content);
-    void sendAttachmentsMail(String to, String subject, String content);
-    void sendHtmlMail(String to, String subject, String content);
-    void sendTemplateMail(String to, String subject);
+    void sendAttachmentsMail(String to, String subject);
 }
